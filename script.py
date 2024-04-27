1efea8dd457805b798763e086269467442687b2f
Aravind Reveendran <37244926+errwnd@users.noreply.github.com>
2024-04-28 04:36:30 +0530

    Update script.py

--- script.py
+++ script.py
@@ -8,6 +8,11 @@ model = load_model("faces.h5")
 #generator = model.compile(optimizer='Adam', loss='sparse_categorical_crossentropy', metrics=['accuracy'])
 
 #generator=model.compile(optimizer="Adam",metrics=['accuracy'],)
+for layer in model.layers:
+    if isinstance(layer, tf.keras.layers.Conv2DTranspose):
+        layer_config = layer.get_config()
+        layer_config.pop('groups', None)
+        layer.__init__(**layer_config)
 
 
 
